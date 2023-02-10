3bb9f1182daf45b9ba205e5f55b8d5a43d0a8826
Kawish Hussain <kawish791@gmail.com>
2023-02-10 22:03:33 +0500

    fix issue where video dont get downloaded

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ytdl = require('youtube-dl-exec');
+const path = require("path");
 const app = express();
 
 app.use(express.json());
@@ -7,11 +8,18 @@ app.use(express.json());
 app.post('/download', (req, res) => {
   console.log("Received request to download video at URL:", req.body.url);
   const url = req.body.url;
-  ytdl.exec(url, ['-f', 'best'], (error, stdout, stderr) => {
+  ytdl.exec(url, ['-o', 'video.mp4'], {}, (error, stdout, stderr) => {
     if (error) {
       return res.status(400).json({error: error.message});
+    } else {
+      const file = path.join(__dirname, 'video.mp4');
+      res.sendFile(file, (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Could not send file');
+        }
+      });
     }
-    res.send(stdout);
   });
 });
 
